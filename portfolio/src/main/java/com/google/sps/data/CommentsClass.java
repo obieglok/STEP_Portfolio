07ddba4ad0c27e75d0f37d0349440b80f7147c13
klaudia obieglo <obieglok@tcd.ie>
2020-07-08 19:26:32 +0000

    Add comments to Comments.java

--- portfolio/src/main/java/com/google/sps/data/CommentsClass.java
+++ portfolio/src/main/java/com/google/sps/data/CommentsClass.java
@@ -17,6 +17,7 @@ package com.google.sps.data;
 import java.util.Date;
 
 public final class CommentsClass{
+    /*Class that creates comments from users input  */
     private final Date currentTime;
     private final String author;
     private final String comment;
@@ -24,21 +25,33 @@ public final class CommentsClass{
 
     public CommentsClass(Date currentTime, String author, String comment,long id)
     {
+        /* Constructor for the comments Class
+        * Arguments: 
+        *  currentTime= Time that the comment was created.
+        *  author = User who wrote the comment.
+        *  comment = The comment the user wrote.
+        *  id = id taken from the entity key value that identifies the comment.
+        *
+         */
         this.currentTime = currentTime;
         this.author = author;
         this.comment = comment;
         this.id = id;
     }
     public Date getCurrentTime(){
+        /*Getter method to get the current time*/
         return currentTime;
     }
     public String getAuthor(){
+        /*Getter method to get the author's name */
         return author;
     }
     public String getComment(){
+        /*Getter method to get the comment */
         return comment;
     }
     public long getId(){
+        /* Getter method to get the comment id*/
         return id;
     }
 }
